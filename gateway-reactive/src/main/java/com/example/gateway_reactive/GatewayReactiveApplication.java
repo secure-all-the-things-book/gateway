926f3e988fee529e0cebd9c9af10fb90affbec7b
Josh Long <joshua.long@broadcom.com>
2026-06-28 21:27:47 +0900

    gateway

---
+++ gateway-reactive/src/main/java/com/example/gateway_reactive/GatewayReactiveApplication.java
@@ -0,0 +1,14 @@
+package com.example.gateway_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayReactiveApplication.class, args);
+	}
+
+
+}
